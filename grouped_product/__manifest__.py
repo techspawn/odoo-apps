d8c902b727a0c4034ff0c4dac5438db83d855b3b
techspawn <siddharth@techspawn.com>
2016-12-12 20:35:27 +0530

    Added Grouped Product for 10.0

---
+++ grouped_product/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Grouped Product",
+
+    'summary': """
+        Module is used to create grouped product in odoo.""",
+
+    'description': """
+        This module is used to create group which will contain multiple product of similar/disimilar type.
+    """,
+
+    'author': "Techspawn Solutions",
+    'website': "http://www.techspawn.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Custom',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale'],
+
+    # always loaded
+    'data': [
+        'views/product_group_view.xml'
+    ],
+
+}
