c85c7a34ff91741663c6c0df8c85f44c7fcb634d
techspawn <siddharth@techspawn.com>
2016-12-12 20:38:20 +0530

    Added Grouped Product for 9.0

---
+++ grouped_product/__openerp__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright © 2016 Techspawn Solutions. (<http://techspawn.in>).
+# 
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': "Grouped Product",
+
+    'summary': """
+        Module is used to create grouped product in odoo.""",
+
+    'description': """
+        This module is used to create group which will contain multiple product of similar/disimilar type. 
+    """,
+
+    'author': "Techspawn Solutions",
+    'website': "http://www.techspawn.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Custom',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale'],
+
+    # always loaded
+    'data': [
+        'views/product_group_view.xml'
+    ],
+
+}
