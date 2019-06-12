8ea85584394aa2c63811e314525a0c65d491bff2
Siddharth <siddharth@techspawn.com>
2019-06-12 11:12:45 +0530

    total_weight_reckoner module migrated

---
+++ total_weight_reckoner/models/__init__.py
@@ -0,0 +1,4 @@
+from .import sale_order_weight
+from .import acc_invoice_weight
+from .import purchase_order_weight
+from .import delivery_order_weight
---
+++ total_weight_reckoner/models/acc_invoice_weight.py
@@ -0,0 +1,39 @@
+from odoo import models, fields, api
+
+class InvoiceWeight(models.Model):
+
+   _inherit = 'account.invoice.line'
+
+   in_weight = fields.Float(string='Gross Weight',
+                               store=True,
+                               related='product_id.weight')
+   # in_net_weight = fields.Float(string='Net Weight',
+   #                             store=True,
+   #                             related='product_id.weight_net')
+   in_quantity = fields.Float(string='Quantity',
+                               store=True,
+                               related='quantity')
+
+class InvoiceWeightOrder(models.Model):
+  
+    _inherit = 'account.invoice'
+    @api.depends('invoice_line_ids.in_weight')
+    @api.depends('invoice_line_ids.in_quantity')
+    # @api.depends('invoice_line_ids.in_net_weight')
+    def _calcweight(self):
+        currentweight = 0
+        for invoice_line in self.invoice_line_ids:
+            currentweight = currentweight + (invoice_line.in_weight * invoice_line.in_quantity)
+
+        self.in_weight_total = currentweight
+
+    in_weight_total = fields.Float(compute='_calcweight', string='Total Gross Weight')
+
+    # def _calcweight_net(self):
+    #     currentweight_net = 0
+    #     for invoice_line in self.invoice_line_ids:
+    #         currentweight_net = currentweight_net + (invoice_line.in_net_weight * invoice_line.in_quantity)
+
+    #     self.in_net_weight_total = currentweight_net
+
+    # in_net_weight_total = fields.Float(compute='_calcweight_net', string='Total Net Weight')
